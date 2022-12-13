0947f9d9834a48b186ace981a83aecf6cb81a266
OlgaSigova <obazukova@gmail.com>
2022-12-14 02:52:00 +0300

    added algorithm

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array1 = new string [5]{"Hello", "2", "world",":-)","bye"};
+string[] array2 = new string [array1.Length];
+int count=0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length<=3)
+    {
+       array2[count]=array1[i];
+       count++; 
+    }
+}
+Console.WriteLine(String.Join(" ", array2));
